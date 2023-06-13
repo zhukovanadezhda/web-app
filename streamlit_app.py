eb80fe7e5eb7228ae1730d43ede65531f94f05e5
Nadezhda Zhukova <nadiajuckova@gmail.com>
2023-06-13 22:18:19 +0300

    added exceptions

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,14 +2,27 @@ import streamlit as st
 import functions as fc
 
 
-st.sidebar.header("Write your request:")
-req = st.sidebar.text_area("Paste your text here")
+st.sidebar.header("Let's look for the info you need")
+req = st.sidebar.text_area("Write your request below:")
 search_button = st.sidebar.button("Search")
 
 if search_button:
-    req_text = fc.call_wiki(req)
-    st.title(f"{req}")
-    st.write(req_text)
+    try:
+        req_text = fc.call_wiki(req)
+        st.title(f"{req}")
+        st.write(req_text)
+    except:
+        st.title(f"{req}")
+        st.write(f"Sorry! I don't have any information about {req}")
+else:
+    st.title(f"Ooops...")
+    st.write("You have to provide a request!")
+    
+    
+
+#col1, col2 = st.columns(2)
+#col1.header("")
+#col1.expander("")
     
     
 
