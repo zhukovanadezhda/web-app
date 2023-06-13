2ed1af93e3c674902e67b4f3567adff544d7b0ef
Nadezhda Zhukova <nadiajuckova@gmail.com>
2023-06-13 21:50:47 +0300

    added sidebar

---
+++ functions.py
@@ -0,0 +1,20 @@
+import requests
+
+def call_wiki(req):
+    url = "https://en.wikipedia.org/w/api.php"
+    params = {
+        "action": "query",
+        "format": "json",
+        "titles": req,
+        "prop": "extracts",
+        "exintro": True,
+        "explaintext": True
+    }
+
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    page_id = next(iter(data["query"]["pages"]))
+    content = data["query"]["pages"][page_id]["extract"]
+
+    return content
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,16 @@
 import streamlit as st
+import functions as fc
 
 st.title("Little Wiki App")
+
+st.sidebar.header("Write your request:")
+req = st.sidebar.text_area("Paste your text here")
+search_button = st.sidebar.button("Search")
+
+if search_button:
+    req_text = fc.call_wiki(req)
+    st.write(req_text)
+    
+    
+
+
