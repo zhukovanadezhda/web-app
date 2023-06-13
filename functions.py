194fe90d6ab3ad4aaf39fa1385fa5b097122a485
Nadezhda Zhukova <nadiajuckova@gmail.com>
2023-06-13 22:33:23 +0300

    added exceptions

--- functions.py
+++ functions.py
@@ -15,7 +15,6 @@ def call_wiki(req):
     data = response.json()
 
     page_id = next(iter(data["query"]["pages"]))
-    content = data["query"]["pages"][page_id]
-    #["extract"]
+    content = data["query"]["pages"][page_id]["extract"]
 
     return content
